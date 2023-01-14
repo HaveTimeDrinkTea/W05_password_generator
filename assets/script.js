bd05589a11962016897486007d54347307ca9b92
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2023-01-14 20:54:33 +0000

    added the randomize  prep user password array function.

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,24 @@
 // indicate that the code should be executed in "strict mode".
 "use strict";
 
-// Array of special characters that can be included in password
+// Array of special characters to be included in password
 var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 
-// Array of numeric characters that can be included in password
+// Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// Array of lowercase characters that can be included in password
+// Array of lowercase characters to be included in password
 var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-// Array of uppercase characters that can be included in password
+// Array of uppercase characters to be included in password
 var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
-var defaultPwdArray = ((numericCharacters.concat(specialCharacters)).concat(lowerCasedCharacters)).concat(upperCasedCharacters);
 
 var pwdLen = "12";
 var isNum = true;
 var isSpec = true;
 var isLower = true;
 var isUpper = true;
-
-var defaultUserOptionArray = [isNum, isSpec, isLower, isUpper];
+var userOptionArray;
 
 // Function to prompt user for password options
 function getPasswordOptions() {
@@ -30,55 +27,56 @@ function getPasswordOptions() {
   isSpec = confirm("Include special characters in your pasword: \n (Click OK to include or click CANCEL to exclude.)");
   isLower = confirm("Include lower case letters in your pasword: \n (Click OK to include or click CANCEL to exclude.)");
   isUpper = confirm("Include upper case letters in your pasword: \n (Click OK to include or click CANCEL to exclude.)");
-  userOptionArray = [isNum, isSpec, isLower, isUpper];
-  console.log(userOptionArray);
-}
+};
 
+getPasswordOptions();
+userOptionArray = [isNum, isSpec, isLower, isUpper];
+console.log(userOptionArray);
 
-var userPwdArray = [null];
+var userPwdArray;
+var defaultPwdArray = ((numericCharacters.concat(specialCharacters)).concat(lowerCasedCharacters)).concat(upperCasedCharacters);
 
 // function for preparing an array of selected characters
 function prepArray() { 
-  for (let i = 0; i < userOptionArray.length; i++) {
-    if (userOptionArray[i]) {
-        switch (i) {
-            case 0:
-              userPwdArray = numChar.concat(finauserPwdArraylarray);
-              console.log("0:", userPwdArray);
-              break;
-            case 1: 
-              userPwdArray = specChar.concat(userPwdArray);
-              console.log("1:", userPwdArray);
-              break;  
-            case 2:
-              userPwdArray = lowerChar.concat(userPwdArray);
-              console.log("2:", userPwdArray);
-              break;
-            case 3:     
-            userPwdArray = upperChar.concat(userPwdArray);
-              console.log("3:", userPwdArray);
-              break;          
+    for (let i = 0; i < userOptionArray.length; i++) {
+        if (userOptionArray[i]) {
+            switch (i) {
+                case 0:
+                    userPwdArray = numericCharacters.concat(userPwdArray);
+                    console.log("0:", userPwdArray);
+                    break;
+                case 1: 
+                    userPwdArray = specialCharacters.concat(userPwdArray);
+                    console.log("1:", userPwdArray);
+                    break;  
+                case 2:
+                    userPwdArray = lowerCasedCharacters.concat(userPwdArray);
+                    console.log("2:", userPwdArray);
+                    break;
+                case 3:     
+                    userPwdArray = upperCasedCharacters.concat(userPwdArray);
+                    console.log("3:", userPwdArray);
+                    break;          
+            }
         }
     }
-  }
-  console.log("before pop");
-  console.log(userPwdArray);
-  
-  userPwdArray.pop();
+};   
 
-  console.log("after pop");
-  console.log(userPwdArray);
-};
+prepArray();
+console.log("before pop");
+console.log(userPwdArray);
+
+userPwdArray.pop();
+console.log("after pop");
+console.log(userPwdArray);
+
 
 
 
 // function for randomly sorting an array of selected characters using the The Fisher Yates Method
 // the for loop in this function is taken from w3schools.com
 
-var pwdArray;
 
-testArray = ["z","25","56", "X", "g","T", "%", "8", "a"];
-console.log("before:", testArray);
 
 function randomSortArray(anyArray) {
   for (let i = anyArray.length -1; i > 0; i--) {
@@ -89,9 +87,15 @@ function randomSortArray(anyArray) {
   }
 }
 
-randomSortArray(testArray);
 
-console.log("after:", testArray);
+
+console.log("before sort");
+console.log(userPwdArray);
+
+randomSortArray(userPwdArray);
+
+console.log("after sort");
+console.log(userPwdArray);
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -101,12 +105,8 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-  getPasswordOptions();
-  console.log(pwdLen);
-  console.log("isNum", isNum);
-  console.log("isSpec",isSpec);
-  console.log("isLower", isLower);
-  console.log("isUpper", isUpper);
+
+
 }
 
 // Get references to the #generate element
